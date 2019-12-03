7103f9855882ed0878a532072edf7cb6fa82fd9d
suhas <suhas@suhas-Lenovo-G50-80>
2019-12-03 14:11:14 +0530

    never mind

--- hello/views.py
+++ hello/views.py
@@ -17,3 +17,7 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+def sim(request):
+    return HttpResponse("Its just a trik")
+
